e08ef80375562aba3ff8a11737d0a9fc53ce409a
Joseph Young <jsyoung0891@gmail.com>
2019-04-09 19:36:59 +0100

    tested river name

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,7 +1,15 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../river.rb")
+
 class RiverTest < MiniTest::Test
 
+  def setup
+    @river1 = River.new("nile")
+  end
+
+  def test_a_river_has_a_name
+    assert_equal("nile", @river1.name)
+  end
 
 end
